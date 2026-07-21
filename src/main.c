3e46cf2063563d9028ee7438ef43ea240b73d2e8
Voosk <milkgolium@gmail.com>
2026-07-21 23:45:37 +0800

    定义代码格式规范，新增全屏控制功能

--- src/main.c
+++ src/main.c
@@ -1,25 +1,55 @@
 #include "raylib.h"
 
+// Function to enable fullscreen mode
+void EnableFullscreen(void) {
+  if (!IsWindowFullscreen()) {
+    ToggleFullscreen();
+  }
+}
+
+// Function to disable fullscreen mode
+void DisableFullscreen(void) {
+  if (IsWindowFullscreen()) {
+    ToggleFullscreen();
+  }
+}
+
+// Function to set fullscreen mode based on a boolean value
+void SetFullscreen(bool enable) {
+  if (IsWindowFullscreen() != enable) {
+    ToggleFullscreen();
+  }
+}
+
 int main(void) {
-    // Initialization
-    const int screenWidth = 1024;
-    const int screenHeight = 768;
+  // Initialization
+  // Set the screen width and height
+  const int screenWidth = 1024;
+  const int screenHeight = 768;
+  // Set the initial fullscreen state
+  bool fullscreenEnabled = true;
 
-    InitWindow(screenWidth, screenHeight, "The Law Of Prey");
-    SetTargetFPS(60);
+  InitWindow(screenWidth, screenHeight, "The Law Of Prey");
+  SetTargetFPS(60);
 
-    while (!WindowShouldClose()) {
-        // Update
-        ;
+  while (!WindowShouldClose()) {
+    // Update
+    // Toggle fullscreen mode when F11 is pressed
+    if (IsKeyPressed(KEY_F11)) fullscreenEnabled = !fullscreenEnabled;
+    // Set the fullscreen state based on the boolean value
+    if (fullscreenEnabled)
+      EnableFullscreen();
+    else
+      DisableFullscreen();
 
-        // Draw
-        BeginDrawing();
-            ClearBackground(BLACK);
-            DrawText("Hello, world!", 0, 0, 60, RED);
-        EndDrawing();
-    }
+    // Draw
+    BeginDrawing();
+    ClearBackground(BLACK);
+    DrawText("Hello, world!", 0, 0, 60, RED);
+    EndDrawing();
+  }
 
-    // De-Initialization
-    CloseWindow();
-    return 0;
+  // De-Initialization
+  CloseWindow();
+  return 0;
 }
