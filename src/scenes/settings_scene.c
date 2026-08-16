2b970be8cb33c405b078bc1e257de7a4aef5c706
Voosk <milkgolium@gmail.com>
2026-08-16 12:07:59 +0800

    完善中文字体加载
    - 引入 Zpix 字体
    - 详细注释
    - 拼接宏加载字符串

--- src/scenes/settings_scene.c
+++ src/scenes/settings_scene.c
@@ -1,6 +1,14 @@
 #include "raylib.h"
 #include "scene_manager.h"
 
+#define TEXT_BTN_START "开始游戏"
+#define TEXT_BTN_LOAD "加载存档"
+#define TEXT_BTN_SETTING "设置（Settings）"
+#define TEXT_BTN_EXIT "退出"
+// 宏拼接字符串
+#define ALL_SETTINGS_TEXT \
+  TEXT_BTN_START TEXT_BTN_LOAD TEXT_BTN_SETTING TEXT_BTN_EXIT
+
 extern Scene CreateMainMenuScene(void);
 
 static void SettingsInit(void);
@@ -8,9 +16,7 @@ static void SettingsUpdate(void);
 static void SettingsDraw(void);
 static void SettingsUnload(void);
 
-static const char* kQuote =
-    "我们度过的每个平凡的日常，也许就是连续发生的奇迹。";
-static Font QuoteFont;
+static Font UIFont;
 
 Scene CreateSettingsScene(void) {
   return (Scene){.name = "Settings",
@@ -21,15 +27,22 @@ Scene CreateSettingsScene(void) {
 }
 
 static void SettingsInit(void) {
+  // codepoint 计数（码点，在 Unicode 标准中一般写作 U+xxxx，U+十六进制数值）
   int codepointCount = 0;
-  int* codepoints = LoadCodepoints(kQuote, &codepointCount);
-  QuoteFont = LoadFontEx(
-      "assets/fonts/otf/GlowSansSC_Normal/GlowSansSC-Normal-Regular.otf", 36,
-      codepoints, codepointCount);
+  // 此指针指向一个存储着所有需要加载的文字的码点
+  int* codepoints = LoadCodepoints(ALL_SETTINGS_TEXT, &codepointCount);
+  // Zpix 像素字体是 12px 的，所以 fontSize 传入 12
+  // 码点数组 codepoints 被传入以后， LoadFontEx
+  // 会使用循环来逐个加载进入纹理，texture 无需单独声明，它被包含在前面声明的
+  // Font 类型的变量里面。
+  UIFont =
+      LoadFontEx("assets/fonts/ttf/zpix.ttf", 12, codepoints, codepointCount);
+  SetTextureFilter(UIFont.texture, TEXTURE_FILTER_POINT);
   UnloadCodepoints(codepoints);
 }
 
 static void SettingsUpdate(void) {
+  // 按 ESC 回到主场景
   if (IsKeyPressed(KEY_ESCAPE)) {
     SceneManagerSwitch(CreateMainMenuScene());
   }
@@ -37,14 +50,16 @@ static void SettingsUpdate(void) {
 
 static void SettingsDraw(void) {
   ClearBackground(DARKGRAY);
-  DrawText("Settings", 20, 20, 40, WHITE);
 
-  const float fontSize = 36.0f;
+  const float fontSize = 24.0f;
   const float spacing = 1.0f;
-  const Vector2 textSize = MeasureTextEx(QuoteFont, kQuote, fontSize, spacing);
+  // 计算文本宽高
+  const Vector2 textSize =
+      MeasureTextEx(UIFont, ALL_SETTINGS_TEXT, fontSize, spacing);
+  // 根据刚刚算出的文本宽高，将文本放在屏幕中央
   const Vector2 textPos = {(GetScreenWidth() - textSize.x) / 2.0f,
                            (GetScreenHeight() - fontSize) / 2.0f};
-  DrawTextEx(QuoteFont, kQuote, textPos, fontSize, spacing, WHITE);
+  DrawTextEx(UIFont, "设置（Settings）", textPos, fontSize, spacing, WHITE);
 }
 
-static void SettingsUnload(void) { UnloadFont(QuoteFont); }
+static void SettingsUnload(void) { UnloadFont(UIFont); }
